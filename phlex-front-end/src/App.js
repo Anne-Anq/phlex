9e2e2abfd4cc18a119bc41600be5eccb037ecd09
Anne Anquetin <anne.anquetin@gmail.com>
2019-02-20 15:23:09 -0700

    add navbar

--- phlex-front-end/src/App.js
+++ phlex-front-end/src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 
+import Navbar from "./components/navbar";
+import ResultPage from "./components/resultPage";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import "./App.css";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar />
+        <Switch>
+          <Route path="/result" render={props => <ResultPage {...props} />} />
+          <Route
+            path="/join"
+            render={props => <h1>I am the Become a photographer page</h1>}
+          />
+          <Route
+            path="/register"
+            render={props => <h1>I am the register page</h1>}
+          />
+          <Route path="/login" render={props => <h1>I am the login page</h1>} />
+          <Route path="/" render={props => <h1>I am the home page</h1>} />
+        </Switch>
       </div>
     );
   }
