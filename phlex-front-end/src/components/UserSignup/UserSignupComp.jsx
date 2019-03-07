3b5bbd4dc084453cf000f0cc0027a0714b59b937
Steven Rothenburger <s.rothenburger1@gmail.com>
2019-03-07 13:55:00 -0700

    styled the userSignup page

--- phlex-front-end/src/components/UserSignup/UserSignupComp.jsx
+++ phlex-front-end/src/components/UserSignup/UserSignupComp.jsx
@@ -1,30 +1,47 @@
 import React, { Component } from 'react';
+import '../../stylesheets/userSignup.css'
 
 class UserSignup extends Component {
 	state = {};
 	render() {
 		return (
-			<div>
+			<div className='body'>
+                
+                <div className='block'>
 				<h1>User Register Form</h1>
-                <br/>
-                <hr/>
+                </div>
+                <div className='fields'>
+                <div className="block">
 				<h2>First Name</h2>
 				<input id="firstName" placeholder='Enter Your First Name'/>
-                <hr/>
+                </div>
+                
+                <div className='block'>
 				<h2>LastName</h2>
 				<input id="lastName" placeholder='Enter Your Last Name'/>
-                <hr/>
+                </div>
+                
+                <div className='block'>
                 <h2>Email</h2>
 				<input type='email 'id="email" placeholder='Enter Your Email'/>
-                <hr/>
+                </div>
+                
+                <div className='block'>
 				<h2>Password</h2>
 				<input type = "password"id="password" placeholder='Enter Your Password'/>
-                <hr/>
+                </div>
+                
+                <div className='block'>
                 <h2>Profile Photo</h2>
                 <input type="file" name="image" accept="image/*"/>
-                <hr/>
                 <br/>
+                </div>
+                </div>
+
+                
+                <div className='block'>
                 <input type="submit" />
+                </div>
 
 			</div>
 		);
