5fcc81ce04a920ef3a3e13a29da61d206dccd89f
Steven Rothenburger <s.rothenburger1@gmail.com>
2019-03-20 13:25:27 -0600

    made some style changes to photgrapher signup and user signup

--- phlex-front-end/src/components/PhotographerSignup/PhotographerSignupComp.jsx
+++ phlex-front-end/src/components/PhotographerSignup/PhotographerSignupComp.jsx
@@ -11,58 +11,54 @@ class PhotographerSignup extends Component {
                 <div className='body'>
 
                     <div className='block title'>
-                        <h3>Photographer Register Form</h3>
-                    </div>
-                    <div className='fields'>
-                        <div className="block">
-                            <h3>First Name</h3>
-                            <input id="firstName" placeholder='Enter Your First Name' className="form-control form-control-danger form"/>
-                        </div>
-
-                        <div className='block'>
-                            <h3>Last Name</h3>
-                            <input id="lastName" placeholder='Enter Your Last Name' className="form-control form-control-danger form"/>
-                        </div>
-
-                        <div className='block'>
-                            <h3>Email</h3>
-                            <input type='email ' id="email" placeholder='Enter Your Email' className="form-control form-control-danger form"/>
-                        </div>
-
-                        <div className='block'>
-                            <h3>Password</h3>
-                            <input type="password" id="password" placeholder='Enter Your Password' className="form-control form-control-danger form"/>
-                        </div>
-
-                        <div className='block'>
-                            <h3>Confirm Password</h3>
-                            <input type="password" id="confirmPassword" placeholder='Enter Your Password' className="form-control form-control-danger form"/>
-                        </div>
-
-                        <div className='block'>
-                            <h3>Upload A Some Sample Photos!</h3>
-                            <input type="file" name="image" accept="image/*" />
-                            <br />
-                        </div>
+                        <h2>Photographer Register Form</h2>
                     </div>
 
-                    <div className='block'>
-                            <h3>Write A Little About Yourself</h3>
-                            <input type="text" id="confirmPassword" placeholder='Enter A Bio' className="form-control form-control-danger form"/>
-                        </div>
-
-                        <div className='block'>
-                            <h3>Whats Your Price Range?</h3>
-                            <input type="range" id="range" min='0' max='1000' step='10' className="form-control form-control-danger form"/>
-                        </div>
-
-
-                    <div className='submit'>
-                        <button
-                        className= 'btn btn-primary'
-                            type="submit"
-                            >Submit</button>
+                    <div className='block title'>
+                        <h4>Register to be a certified Phlex photographer to start delivering quality and meaningful photography solutions.</h4>
                     </div>
+
+                    <div id='fields'>
+
+                    <div class="form-row">
+						<div class="form-group col-md-6">
+							<label for="inputEmail4">Email</label>
+							<input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+						</div>
+						<div class="form-group col-md-6">
+							<label for="inputPassword4">Password</label>
+							<input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label for="inputCity">City</label>
+							<input type="text" class="form-control" id="inputCity" placeholder='City' />
+						</div>
+						<div class="form-group col-md-4">
+							<label for="inputState">Province</label>
+							<select id="inputState" class="form-control">
+								<option selected>Choose...</option>
+								<option>Alberta</option>
+                                <option>British Columbia</option>
+                                <option>Manitoba</option>
+                                <option>New Brunswick</option>
+                                <option>Newfoundland and Labrador</option>
+                                <option>Northwest Territories</option>
+                                <option>Nova Scotia</option>
+                                <option>Nunavut</option>
+                                <option>Ontario</option>
+                                <option>PEI</option>
+                                <option>Quebec</option>
+                                <option>Saskatchewan</option>
+                                <option>Yukon</option>
+							</select>
+						</div>
+					</div>
+                    </div>
+					<button type="submit" class="btn btn-primary">
+						Sign up
+					</button>
 
                 </div>
             )
